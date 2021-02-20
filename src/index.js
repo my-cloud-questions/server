d03aa0343656907242a186e28b8cf11e44187e7a
Abid <abidfs@gmail.com>
2021-02-20 22:13:10 +0000

    Using latest aws serverless express package

--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
 "use strict";
 process.env.EXECUTION_ENV_SERVERLESS = true;
 
-const awsServerlessExpress = require("aws-serverless-express");
+const awsServerlessExpress = require("@vendia/serverless-express");
 const app = require("./app").app;
-const server = awsServerlessExpress.createServer(app);
 
-const handler = (event, context) => {
-  return awsServerlessExpress.proxy(server, event, context);
-};
-
-exports.handler = handler;
+exports.handler = awsServerlessExpress({ app });
