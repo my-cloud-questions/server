ee34964e805683a1045e25dc67bcf0fe3ae46bef
Abid <abidfs@gmail.com>
2021-03-03 22:20:04 +0000

    Added unit tests for index.js

--- src/index.test.js
+++ src/index.test.js
@@ -1,6 +1,20 @@
-/* eslint-disable no-unused-vars */
+const mockAwsServerlessExpress = jest.fn((param) => param);
+jest.mock("@vendia/serverless-express", () => {
+  return (params) => jest.fn(() => mockAwsServerlessExpress(params));
+});
 
-import index from "./index";
+const mockApp = jest.fn();
+jest.mock("./app", () => {
+  return { app: mockApp };
+});
 
-test("it should set EXECUTION_ENV_SERVERLESS environment variable to true", () =>
-  expect(global.process.env.EXECUTION_ENV_SERVERLESS).toBeTruthy());
+test("it should set EXECUTION_ENV_SERVERLESS environment variable to true", () => {
+  require("./index");
+  return expect(global.process.env.EXECUTION_ENV_SERVERLESS).toBeTruthy();
+});
+
+test("it should call awsServerlessExpress", () => {
+  const handler = require("./index").handler;
+  handler();
+  expect(mockAwsServerlessExpress).toHaveBeenCalledWith({ app: mockApp });
+});
