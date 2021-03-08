3632bec19ca11c56c03f4526ecfd85a6fafd93d8
Abid <abidfs@gmail.com>
2021-03-08 22:40:12 +0000

    Added logs in health API

--- src/routes/health/health.js
+++ src/routes/health/health.js
@@ -1,14 +1,15 @@
 const health = (req, res, next) => {
   try {
+    req.logger.addEvent("health start");
     res.status(200);
     res.json({
       status: "Healthy"
     });
+    req.logger.addEvent("send response");
+    req.logger.done();
   } catch (e) {
     next(e);
   }
-  // req.logger.addEvent("health check");
-  // req.logger.done();
 };
 
 export default health;
