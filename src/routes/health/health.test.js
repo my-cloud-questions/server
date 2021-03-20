73bf73dd05531d15406b3486813a9a3c3f2f72cc
ankitcs11 <ankitcs11@gmail.com>
2021-03-21 05:06:46 +0530

    health unit test

--- src/routes/health/health.test.js
+++ src/routes/health/health.test.js
@@ -1,1 +1,21 @@
-test.todo("to be done");
+import health from ".";
+
+test("it should log events", () => {
+  health(mockRequest, mockResponse, mockNext);
+  expect(mockRequest.logger.addEvent).toHaveBeenCalledTimes(2);
+  expect(mockRequest.logger.addEvent).toHaveBeenNthCalledWith(1, "Started");
+  expect(mockRequest.logger.addEvent).toHaveBeenNthCalledWith(
+    2,
+    "Send response"
+  );
+  expect(mockRequest.logger.done).toHaveBeenCalledTimes(1);
+});
+
+test("it should set response status", () => {
+  health(mockRequest, mockResponse, mockNext);
+  expect(mockResponse.status).toHaveBeenCalledWith(200);
+});
+test("it should set response json", () => {
+  health(mockRequest, mockResponse, mockNext);
+  expect(mockResponse.json).toHaveBeenCalledWith({ status: "Healthy" });
+});
